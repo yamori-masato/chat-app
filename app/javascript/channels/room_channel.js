5694d685f98d0ed68b3a12034e9232344b1ef10f
yamori-masato <yamorimasato@gmail.com>
2020-11-19 02:42:28 +0900

    画面遷移毎に購読される問題を修正

--- app/javascript/channels/room_channel.js
+++ app/javascript/channels/room_channel.js
@@ -5,22 +5,31 @@ document.addEventListener("turbolinks:load", () => {
   if (data === null) {
     return
   }
+  const channel = "RoomChannel"
   const room_id = data.getAttribute("data-room-id")
   const user_id = data.getAttribute("data-user-id")
-
-  consumer.subscriptions.create(
-    { channel: "RoomChannel", room_id: room_id, user_id: user_id },
-    {
-      connected() {
-      },
-
-      disconnected() {
-      },
-
-      received(data) {
-        const container = document.getElementById("container")
-        container.insertAdjacentHTML('beforeend', data['message'])
+  if (!is_subscribed(channel, room_id, user_id)) {
+    consumer.subscriptions.create(
+      { channel: channel, room_id: room_id, user_id: user_id },
+      {
+        connected() {
+        },
+  
+        disconnected() {
+        },
+  
+        received(data) {
+          const container = document.getElementById("container")
+          container.insertAdjacentHTML('beforeend', data['message'])
+        }
       }
-    }
-  )
+    )
+  }
 })
+
+// helper
+const is_subscribed = (channel, room_id, user_id) => {
+  const identifier = `{"channel":"${channel}","room_id":"${room_id}","user_id":"${user_id}"}`
+  const subscription = consumer.subscriptions.findAll(identifier)
+  return !!subscription.length
+}
